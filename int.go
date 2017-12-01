c5e412fc8ac1b6e12aea28b2c53b90be8054fb73
fibo <casati_gianluca@yahoo.it>
2017-12-01 11:59:43 +0100

    working implementation, needs only to add the list

--- int.go
+++ int.go
@@ -1,5 +1,27 @@
 package primes
 
 func IntIsPrime(n int) bool {
-	return true
+	isPrime := map[int]bool{
+		1:  true,
+		2:  true,
+		3:  true,
+		5:  true,
+		7:  true,
+		11: true,
+		13: true,
+		17: true,
+		19: true,
+		23: true,
+		29: true,
+		31: true,
+		37: true,
+		41: true,
+		43: true,
+		47: true,
+		53: true,
+		59: true,
+		61: true,
+	}
+
+	return isPrime[n]
 }
